ba96839c296e1323e2de7705f5e96db09ac921d2
Hana Lee <leehana+4@berkeley.edu>
2023-02-24 13:44:45 -0800

    suggested person card component

--- app/pages/explore/SuggestedPersonCard.tsx
+++ app/pages/explore/SuggestedPersonCard.tsx
@@ -1,27 +1,24 @@
 import React from 'react';
-import { Image, Text, View, StyleSheet } from 'react-native';
+import { Image, ImageBackground, Text, View, StyleSheet } from 'react-native';
 
 export default function SuggestedPersonCard() {
   return (
-    <View style={styles.Container}>
-     <Image source={{uri: 'https://i0.wp.com/newspack-berkeleyside-cityside.s3.amazonaws.com/wp-content/uploads/2016/09/OskiRappels500.jpg'}} style={styles.ProfileImage} />
-     <View style={styles.StatusView}>
-     <Text style={styles.StatusText}>Local</Text>
-     </View>
-      <Image source={{uri: 'https://cdn.britannica.com/46/7046-004-BB1F8E32/state-flag-Bear-Flag-California-red-star-July-9-1846.jpg'}} style={styles.FlagImage} />
-      <Text style={styles.NameText}>First Name</Text>
-    </View>
+     <View style={styles.Container}>
+     <ImageBackground source={{uri: 'https://i0.wp.com/newspack-berkeleyside-cityside.s3.amazonaws.com/wp-content/uploads/2016/09/OskiRappels500.jpg'}} imageStyle={styles.ProfileImage}>
+      <View style={{flex: 1, flexDirection: 'row', justifyContent: 'space-between', alignItems: 'flex-end', top: 182}}>
+        <View style={styles.StatusView}><Text style={styles.StatusText}>Local</Text></View>
+        <Image source={{uri: 'https://pbs.twimg.com/media/ET0Tyl-UUAACA-a?format=jpg'}} style={styles.FlagImage} />
+      </View>
+      </ImageBackground>
+      <View style={{position:'absolute', bottom: 0}}><Text style={styles.NameText}>First Name</Text></View></View>
   );
 }
 
 const styles = StyleSheet.create({
   Container: {
     position: 'absolute',
-    width: 200,
-    height: 296,
-    flex: 1,
-    justifyContent: 'center',
-    backgroundColor: 'transparent',
+    width: 172,
+    height: 258,
   },
   ProfileImage: {
     width: 172,
@@ -31,36 +28,30 @@ const styles = StyleSheet.create({
     borderColor: 'white',
   },
   FlagImage: {
-    height: 33,
     width: 33,
+    height: 33,
     borderRadius: 22,
     borderWidth: 2,
     borderColor: 'white',
-    top: -65,
-    left: 130,
+    alignSelf: 'right',
+    marginHorizontal: 12,
   },
   StatusView: {
     height: 22,
     width: 94,
     borderRadius: 5,
     backgroundColor: '#8F6FE0',
-    top: -35,
-    left: 10,
+    marginHorizontal: 8,
   },
-  StatusText: {
+  StatusText: {   
+    alignSelf: 'center',
     color: 'white',
-    fontStyle: 'normal',
     fontSize: 12,
-    display: 'flex',
-    letterSpacing: 0.055,
     padding: 3,
-    textAlign: 'center',
   },
   NameText: {
     color: 'white',
-    fontStyle: 'normal',
     fontSize: 20,
-    letterSpacing: 0.055,
-    top: -50,
+    marginVertical: 5,
   }
 });
