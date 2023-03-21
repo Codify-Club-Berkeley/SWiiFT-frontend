60323dd4954e187ddafa7a074c10691672379b9a
aidansunbury <aidansunbury@gmail.com>
2023-03-20 18:17:09 -0700

    updated login screens from login branch

--- app/pages/setup-and-login/Login.tsx
+++ app/pages/setup-and-login/Login.tsx
@@ -1,33 +1,64 @@
 import {
   StyleSheet,
   Text,
+  ImageBackground,
+  TextInput,
   View,
   Image,
   TouchableOpacity,
-  ImageBackground,
 } from "react-native";
 
 export default function Login({ navigation }) {
+  const onFooterLinkPressNav = () => {
+    navigation.navigate("SignUp");
+  };
+  const onForgotLinkPressNav = () => {
+    navigation.navigate("MainPage");
+  };
+
   return (
     <View style={styles.container}>
       <ImageBackground
+        style={styles.background}
         source={require("../../assets/images/login-images/beach.png")}
-        resizeMode="cover"
-        style={{ flex: 1, justifyContent: "center", width: "100%" }}
       >
-        <View style={styles.container}>
-          <Image
-            style={styles.image}
-            source={require("../../assets/images/swiift-small-logo.png")}
-            resizeMode="contain"
-          />
-          <Text style={styles.text}>Welcome back!</Text>
-          <TouchableOpacity
-            style={styles.loginButton}
-            onPress={() => navigation.navigate("Tab")}
-          >
-            <Text style={styles.loginText}>LOG IN</Text>
-          </TouchableOpacity>
+        <Image
+          style={styles.image}
+          source={require("../../assets/images/swiift-small-logo.png")}
+          resizeMode="contain"
+        />
+        <Text style={styles.text}>Welcome back!</Text>
+        <TextInput
+          style={styles.input}
+          placeholder="Email"
+          placeholderTextColor="#2D2C2E"
+          underlineColorAndroid="transparent"
+          autoCapitalize="none"
+        />
+        <TextInput
+          style={styles.input}
+          placeholderTextColor="#2D2C2E"
+          secureTextEntry
+          placeholder="Password"
+          underlineColorAndroid="transparent"
+          autoCapitalize="none"
+        />
+        <Text onPress={onForgotLinkPressNav} style={styles.forgot}>
+          Forgot password?
+        </Text>
+        <TouchableOpacity
+          style={styles.loginButton}
+          onPress={() => navigation.navigate("Tab")}
+        >
+          <Text style={styles.loginText}>LOG IN</Text>
+        </TouchableOpacity>
+        <View style={styles.footerView}>
+          <Text style={styles.footerText}>
+            Don't have an account?{" "}
+            <Text onPress={onFooterLinkPressNav} style={styles.footerLink}>
+              Sign up
+            </Text>
+          </Text>
         </View>
       </ImageBackground>
     </View>
@@ -35,25 +66,57 @@ export default function Login({ navigation }) {
 }
 
 const styles = StyleSheet.create({
+  background: {
+    flex: 1,
+    resizeMode: "cover",
+    justifyContent: "center",
+    width: 400,
+  },
   container: {
     flex: 1,
     alignItems: "center",
     flexDirection: "column",
+    backgroundColor: "#2D2C2E",
+  },
+  forgot: {
+    color: "white",
+    alignSelf: "flex-end",
+    marginRight: 20,
   },
   image: {
-    width: "30%",
-    height: "30%",
+    marginTop: 140,
+    alignSelf: "center",
+    width: 100,
+    height: 100,
   },
   text: {
-    alignSelf: "left",
+    alignSelf: "center",
     paddingHorizontal: 20,
     color: "white",
-    fontSize: 50,
+    fontSize: 40,
     fontWeight: "bold",
-    paddingTop: 10,
+    marginVertical: 20,
+  },
+  input: {
+    alignSelf: "center",
+    height: 60,
+    width: 350,
+    borderRadius: 5,
+    borderColor: "white",
+    overflow: "hidden",
+    backgroundColor: "white",
+    opacity: 0.7,
+    marginTop: 10,
+    marginBottom: 10,
+    marginLeft: 30,
+    marginRight: 30,
+    fontSize: 19,
+    paddingLeft: 16,
+    placeholderTextColor: "#2D2C2E",
   },
   loginButton: {
-    width: "90%",
+    alignSelf: "center",
+    width: 350,
     borderRadius: 12,
     borderWidth: 3,
     borderColor: "white",
@@ -61,17 +124,25 @@ const styles = StyleSheet.create({
     alignItems: "center",
     justifyContent: "center",
     backgroundColor: "#9B6EB7",
-    marginTop: 15,
+    marginTop: 70,
   },
-  signUpText: {
+  loginText: {
     alignSelf: "center",
     color: "white",
     fontSize: 20,
   },
+  footerView: {
+    flex: 1,
+    alignItems: "center",
+    marginTop: 120,
+  },
+  footerText: {
+    fontSize: 16,
+    color: "white",
+  },
+  footerLink: {
+    color: "#9B6EB7",
+    fontWeight: "bold",
+    fontSize: 16,
+  },
 });
-
-// /**
-//  * Fix for "navigation implicitly has type any"
-//  * https://stackoverflow.com/questions/63132548/react-navigation-5-error-binding-element-navigation-implicitly-has-an-any-ty
-//  * https://reactnavigation.org/docs/typescript/
-//  */
--- app/pages/setup-and-login/SignUp.tsx
+++ app/pages/setup-and-login/SignUp.tsx
@@ -1,33 +1,58 @@
 import {
   StyleSheet,
+  ImageBackground,
   Text,
+  TextInput,
   View,
   Image,
   TouchableOpacity,
-  ImageBackground,
 } from "react-native";
 
 export default function SignUp({ navigation }) {
+  const onFooterLinkPressNav = () => {
+    navigation.navigate("Login");
+  };
+
   return (
     <View style={styles.container}>
       <ImageBackground
+        style={styles.background}
         source={require("../../assets/images/login-images/mountain.png")}
-        resizeMode="cover"
-        style={{ flex: 1, justifyContent: "center", width: "100%" }}
       >
-        <View style={styles.container}>
-          <Image
-            style={styles.image}
-            source={require("../../assets/images/swiift-small-logo.png")}
-            resizeMode="contain"
-          />
-          <Text style={styles.text}>Create Account</Text>
-          <TouchableOpacity
-            style={styles.loginButton}
-            onPress={() => navigation.navigate("Tab")}
-          >
-            <Text style={styles.loginText}>SIGN UP</Text>
-          </TouchableOpacity>
+        <Image
+          style={styles.image}
+          source={require("../../assets/images/swiift-small-logo.png")}
+          resizeMode="contain"
+        />
+        <Text style={styles.text}>Create Account</Text>
+        <TextInput
+          style={styles.input}
+          placeholder="Email"
+          placeholderTextColor="#2D2C2E"
+          underlineColorAndroid="transparent"
+          autoCapitalize="none"
+        />
+        <TextInput
+          style={styles.input}
+          placeholderTextColor="#2D2C2E"
+          secureTextEntry
+          placeholder="Password"
+          underlineColorAndroid="transparent"
+          autoCapitalize="none"
+        />
+        <TouchableOpacity
+          style={styles.signUpButton}
+          onPress={() => navigation.navigate("Tab")}
+        >
+          <Text style={styles.signUpText}>SIGN UP</Text>
+        </TouchableOpacity>
+        <View style={styles.footerView}>
+          <Text style={styles.footerText}>
+            Already have an account?{" "}
+            <Text onPress={onFooterLinkPressNav} style={styles.footerLink}>
+              Log in
+            </Text>
+          </Text>
         </View>
       </ImageBackground>
     </View>
@@ -35,26 +60,52 @@ export default function SignUp({ navigation }) {
 }
 
 const styles = StyleSheet.create({
+  background: {
+    flex: 1,
+    resizeMode: "cover",
+    justifyContent: "center",
+    width: 400,
+  },
   container: {
     flex: 1,
-
     alignItems: "center",
     flexDirection: "column",
+    backgroundColor: "#2D2C2E",
   },
   image: {
-    width: "30%",
-    height: "30%",
+    marginTop: 140,
+    alignSelf: "center",
+    width: 100,
+    height: 100,
   },
   text: {
-    alignSelf: "left",
+    alignSelf: "center",
     paddingHorizontal: 20,
     color: "white",
-    fontSize: 50,
+    fontSize: 40,
     fontWeight: "bold",
-    paddingTop: 10,
+    marginVertical: 20,
   },
-  loginButton: {
-    width: "90%",
+  input: {
+    alignSelf: "center",
+    height: 60,
+    width: 350,
+    borderRadius: 5,
+    borderColor: "white",
+    overflow: "hidden",
+    backgroundColor: "white",
+    opacity: 0.7,
+    marginTop: 10,
+    marginBottom: 10,
+    marginLeft: 30,
+    marginRight: 30,
+    fontSize: 19,
+    paddingLeft: 16,
+    placeholderTextColor: "#2D2C2E",
+  },
+  signUpButton: {
+    alignSelf: "center",
+    width: 350,
     borderRadius: 12,
     borderWidth: 3,
     borderColor: "white",
@@ -62,11 +113,25 @@ const styles = StyleSheet.create({
     alignItems: "center",
     justifyContent: "center",
     backgroundColor: "#9B6EB7",
-    marginTop: 15,
+    marginTop: 70,
   },
   signUpText: {
     alignSelf: "center",
     color: "white",
     fontSize: 20,
+  },
+  footerView: {
+    flex: 1,
+    alignItems: "center",
+    marginTop: 120,
+  },
+  footerText: {
+    fontSize: 16,
+    color: "white",
+  },
+  footerLink: {
+    color: "#9B6EB7",
+    fontWeight: "bold",
+    fontSize: 16,
   },
 });
