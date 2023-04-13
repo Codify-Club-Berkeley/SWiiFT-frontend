31bd52f8b89d0ca85c4aba0289eecc237c757889
Sydney Tsai <sydneyctsai@gmail.com>
2023-04-12 19:14:31 -0700

    start

--- app/pages/setup-and-login/Start.js
+++ app/pages/setup-and-login/Start.js
@@ -1,13 +1,26 @@
 import React from 'react';
 import { StyleSheet, Text, View, TextInput, TouchableOpacity } from 'react-native';
+import { SelectList } from 'react-native-dropdown-select-list';
 
 export default function Start({navigation}) {
   const onNextPressNav = () => {
     navigation.navigate('Start')
   }
   const [text, onChangeText] = React.useState('Enter your first name');
+  const [selected, setSelected] = React.useState("");
+  const data1 = [
+    {key:'1', value:'Select gender', disabled:true},
+    {key:'2', value:'Male'},
+    {key:'3', value:'Female'},
+    {key:'4', value:'Nonbinary'},
+  ]
+  const data2 = [
+    {key:'1', value:'Country', disabled:true},
+    {key:'2', value:'United States'},
+    {key:'3', value:'Italy'},
+    {key:'4', value:'China'},
+  ]
 
-  
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Let's start basic</Text>
@@ -30,7 +43,23 @@ export default function Start({navigation}) {
         onChangeText = {onChangeText}
       ></TextInput>
       <Text style={styles.questions}>I identify as...</Text>
+      <SelectList
+        setSelected={setSelected}
+        data={data1}  
+        search={false} 
+        boxStyles={{borderWidth:0, borderRadius:30, width:"65%", backgroundColor:"#4D4C4D"}}
+        defaultOption={{ key:'1', value:'Select gender' }}
+        placeholderTextColor="white"
+      />
       <Text style={styles.questions}>My country of origin is...</Text>
+      <SelectList
+        setSelected={setSelected}
+        data={data2}  
+        search={false} 
+        boxStyles={{borderWidth:0, borderRadius:30, width:"60%", backgroundColor:"#4D4C4D"}}
+        defaultOption={{ key:'1', value:'Country' }}
+        placeholderTextColor="white"
+      />
     </View>
   );
 }
@@ -48,6 +77,7 @@ const styles = StyleSheet.create({
     fontSize: 30,
     alignSelf: "left",
     paddingLeft: 20,
+    padding: 30,
   },
   questions: {
     color: "white",
